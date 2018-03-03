e0fc9a2b6e9dd70b7281a1f75814a40aa9b3a46f
Gabriel <gabrielnoya95@gmail.com>
2018-03-02 23:47:33 -0400

    union codigos

--- Glbp.py
+++ Glbp.py
@@ -2,25 +2,45 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 
-matrix = np.array([[180, 176,168], [179, 175,170],[169,174,170]])
+def consecutive(data, stepsize=1):
+    return np.split(data, np.where(np.diff(data) != stepsize)[0]+1)
+
+matrix = np.array([[180, 176,168], [179, 175,170],[169,174,180]])
 matrixB = matrix > matrix[1][1]
-matrixB=matrixB.astype(int)
+matrixB = matrixB.astype(int)
 
 
-a=np.zeros(8,np.int8)
-numberShift=0
-a[0]=matrixB[1][2]
-a[1]=matrixB[0][2]
-a[2]=matrixB[0][1]
-a[3]=matrixB[0][0]
-a[4]=matrixB[1][0]
-a[5]=matrixB[2][0]
-a[6]=matrixB[2][1]
-a[7]=matrixB[2][2]
-if (np.count_nonzero(a)!=8):
-    while(a[7]==1):
-        a=np.roll(a,1)
-        numberShift+=1
-print(a)
-print(np.flatnonzero(a))
+a = np.zeros(8,np.int8)
+numberShift = 0
 
+a[0] = matrixB[1][2]
+a[1] = matrixB[0][2]
+a[2] = matrixB[0][1]
+a[3] = matrixB[0][0]
+a[4] = matrixB[1][0]
+a[5] = matrixB[2][0]
+a[6] = matrixB[2][1]
+a[7] = matrixB[2][2]
+
+if (np.count_nonzero(a) != 8):
+    while(a[7] == 1):
+        a = np.roll(a,1)
+        numberShift += 1
+
+nonZeroIndexes = np.flatnonzero(a)
+cons = consecutive(nonZeroIndexes)
+
+vector = np.zeros((4, 8))
+
+for i in range(0, len(cons)):
+    vector[i][cons[i]] = 1
+
+    if(np.count_nonzero(vector[i]) % 2 != 0):
+        indexes = np.flatnonzero(vector[i])
+        middle = int((indexes[-1] + indexes[0] ) / 2)
+        angle = middle - numberShift
+        if(angle < 0):
+            angle += 8
+        width = np.count_nonzero(vector[i])
+        print(width)
+        print(angle)
