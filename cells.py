feb73bc6fcb2f998843fbfb8ecb5205c4c0839db
Gabriel <gabrielnoya95@gmail.com>
2018-03-05 07:49:17 -0400

    celdas no funcionando correct

--- cells.py
+++ cells.py
@@ -6,9 +6,15 @@ fileName = 'test.png'
 
 image = cv2.imread(fileName)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-total = 
+total = []
+i = 0
+
 for i in range(0, 128, 16):
     for j in range(0, 64, 16):
-        a = image[i:i+2, j:j+2]
-        tempTable = glbp.glbpTable(a)
-        total.append(tempTable)
+        a = image[i:i+16, j:j+16]
+        i += 1
+        # tempTable = glpb.glbpTable(a)
+        # total = np.append(total, tempTable)
+
+print(i)
+# print(len(total))
