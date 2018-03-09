e2cc8ddd64c26537eeb61047676875b9fcbb3a63
Gabriel <gabrielnoya95@gmail.com>
2018-03-09 17:40:44 -0400

    codigo bonito

--- cells.py
+++ cells.py
@@ -2,41 +2,33 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import Glbp as glpb
-fileName = 'test.png'
-fileName2 = 'test1.png'
-fileName3 = 'background.png'
+import time
 
 def finalHistogram(img):
     total = []
     for i in range(0, 128-8, 8):
         for j in range(0, 64-8, 8):        
-            a = img[i:i+15, j:j+15]
-            tempTable = glpb.glbpTable(a)
+            tempTable = glpb.glbpTable(img[i:i+15, j:j+15])
             total = np.append(total, tempTable)
-            
-    return total
+    norm = np.linalg.norm(total, 2)
+    return np.divide(total, norm)
 
+fileName = 'test.png'
+fileName2 = 'test1.png'
+fileName3 = 'background.png'
 
 image = cv2.imread(fileName)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# image2 = cv2.imread(fileName2)
-# image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
-
-# image3 = cv2.imread(fileName3)
-# image3 = cv2.cvtColor(image3, cv2.COLOR_BGR2GRAY)
+t0 = time.time()
 
 final = finalHistogram(image)
-finalNorm = np.linalg.norm(final, 2)
-finalNorm = np.divide(final, finalNorm)
-# final2 = finalHistogram(image2)
-# final3 = finalHistogram(image3) 
 
-plt.figure(1)
-plt.bar(range(len(final)), final, color='r', align='center')
-plt.figure(2)
-plt.bar(range(len(finalNorm)), finalNorm, color='r', align='center')
+t1 = time.time()
 
-# plt.figure(3)
-# plt.bar(range(len(final3)), final3, color='r', align='center')
-plt.show()
+print(t1-t0)
+
+# plt.figure(1)
+# plt.bar(range(len(final)), final, color='r', align='center')
+
+# plt.show()
