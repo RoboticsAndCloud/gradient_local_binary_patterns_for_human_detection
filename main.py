91cab2802588806787b6b8a57ad48b6a35296d52
Gabriel <gabrielnoya95@gmail.com>
2018-03-09 19:10:01 -0400

    añadidas imagenes de prueba

--- main.py
+++ main.py
@@ -6,17 +6,47 @@ import time
 
 
 
-fileName = './images/test.png'
-fileName2 = './images/test1.png'
-fileName3 = './images/background.png'
+fileName = './images/person/person1.png'
+fileName = './images/person/person2.png'
+fileName = './images/person/person3.png'
+fileName = './images/backgrounds/bg1.png'
+fileName = './images/backgrounds/bg2.png'
+fileName = './images/backgrounds/bg3.png'
 
+fileName = './images/person/person1.png'
 image = cv2.imread(fileName)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-t0 = time.time()
 final = glpb.finalHistogram(image)
 glpb.printGLBPHistogram(final, 1)
-t1 = time.time()
 
-print(t1-t0)
+fileName = './images/person/person2.png'
+image = cv2.imread(fileName)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+final = glpb.finalHistogram(image)
+glpb.printGLBPHistogram(final, 2)
+
+fileName = './images/person/person3.png'
+image = cv2.imread(fileName)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+final = glpb.finalHistogram(image)
+glpb.printGLBPHistogram(final, 3)
+
+fileName = './images/backgrounds/bg1.png'
+image = cv2.imread(fileName)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+final = glpb.finalHistogram(image)
+glpb.printGLBPHistogram(final, 4)
+
+fileName = './images/backgrounds/bg2.png'
+image = cv2.imread(fileName)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+final = glpb.finalHistogram(image)
+glpb.printGLBPHistogram(final, 5)
+
+fileName = './images/backgrounds/bg3.png'
+image = cv2.imread(fileName)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+final = glpb.finalHistogram(image)
+glpb.printGLBPHistogram(final, 6)
+
 plt.show()
