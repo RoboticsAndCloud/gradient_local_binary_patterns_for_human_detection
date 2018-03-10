d3b7cdee1831f576c4e768d820437d404f1598fc
Gabriel <gabrielnoya95@gmail.com>
2018-03-10 02:22:18 -0400

    train py y leer

--- main.py
+++ main.py
@@ -2,34 +2,38 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import Glbp as glpb
+import Train as train
 import time
 
-svm = cv2.ml.SVM_create()
-svm.setType(cv2.ml.SVM_C_SVC)
-svm.setKernel(cv2.ml.SVM_LINEAR)
 
-# Machine learning
+# h = np.loadtxt('outBG.txt', dtype='str')
 
-# trainData = np.array([final, final2, final3, final7, final7, final7], dtype=np.float32)
-# trainLabels = np.array([1, 1, 1, 0, 0, 0], dtype=np.float32)
+# out = []
 
-# testData = np.array([final, final7], dtype=np.float32)
-
-h = np.loadtxt('outBG.txt', dtype='str')
-out = []
-
-for files in h:
-    fileName = ('./images/Background/%s' % files)
-    print(fileName)
-    image = cv2.imread(fileName)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    final = glpb.finalHistogram(image)
-    out = np.append(out, final)
+# for files in h:
+#     fileName = ('./images/Background/%s' % files)
+#     print(fileName)
+#     image = cv2.imread(fileName)
+#     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     final = glpb.finalHistogram(image)
+#     out = np.append(out, final)
     
-np.savetxt('./txt/hogBG.txt', out)
+# np.savetxt('./txt/hogBG.txt', out)
 
 
-# svm.save('svm_data.dat')
 # testResponse = svm.predict(testData)[1].ravel()
 # print(testResponse)
-# plt.show()
+
+persons = np.loadtxt('/txt/persons.txt', dtype='str')
+backgrounds = np.loadtxt('/txt/backgrounds.txt', dtype='str')
+
+persons = np.reshape(persons, (-1, 5880))
+backgrounds = np.reshape(backgrounds, (-1, 5880))
+hog = np.append(persons, backgrounds)
+
+responsesPersons = np.ones(len(persons), np.uint8)
+responsesBackgrounds = np.ones(len(backgrounds), np.uint8)
+responses = np.append(responsesPersons, responsesBackgrounds)
+
+svm = train.createSVM()
+train.train(svm, hog, responses)
