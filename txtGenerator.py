62a106674dd28acdceab30b276286647af4f7fbc
Jeck Jaimes <jeckjaimes@gmail.com>
2018-03-10 01:45:44 -0400

    Generador de textos hog

--- txtGenerator.py
+++ txtGenerator.py
@@ -5,14 +5,21 @@ import Glbp as glpb
 import time
 import os
 
-files = os.listdir("../../Curso detección de objetos/Datasets/Pedestrians-Dataset-Dummy/Pedestrians/")
+files = os.listdir("./images/person")
 
 np.savetxt('out.txt', files, fmt='%s')
 
 h = np.loadtxt('out.txt', dtype='str')
 
-fileName = ('../../Curso detección de objetos/Datasets/Pedestrians-Dataset-Dummy/Pedestrians/test1.png')
-print(fileName)
-image = cv2.imread(fileName)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-final = glpb.finalHistogram(image)
+# print(h)
+out = []
+
+for file in h:
+    fileName = ('./images/person/%s' % file)
+    print(fileName)
+    image = cv2.imread(fileName)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    final = glpb.finalHistogram(image)
+    out = np.append(out, final)
+    
+np.savetxt('hog.txt', out)
