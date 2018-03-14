2a16d00797696a1530d5a81374225f00c6c691d9
Gabriel <gabrielnoya95@gmail.com>
2018-03-14 11:03:54 -0400

    implementado 2/3

--- main.py
+++ main.py
@@ -3,37 +3,60 @@ import numpy as np
 from matplotlib import pyplot as plt
 import Glbp as glpb
 import train
+import math
 
+# Leemos los archivos binarios
+persons = np.load('./txt/PData_bin.npy')
+persons = np.reshape(persons, (-1, 5880))
+print('Persons read')
+backgrounds = np.load('./txt/BGData_bin.npy')
+backgrounds = np.reshape(backgrounds, (-1, 5880))
+print('Backgrounds read')
+print('Entranando con los ultimos 2/3')
 
-def getArrays():
-    # Leemos los archivos binarios
-    persons = np.load('./txt/PData_bin.npy')
-    print('Persons read')
-    backgrounds = np.load('./txt/BGData_bin.npy')
-    print('Backgrounds read')
+trainPersonsMin = math.floor(len(persons)*1/3)
+trainPersonsMax = len(persons)
+testPersonsMin = 0
+testPersonsMax = math.floor(len(persons)*1/3)
 
-    persons2len = len(np.reshape(persons, (-1, 5880)))
-    backgrounds2len = len(np.reshape(backgrounds, (-1, 5880)))
-    hog = np.append(persons, backgrounds)
-    persons = None
-    backgrounds = None
-    hog = np.reshape(hog, (-1,5880))    
+trainBackgroundsMin = math.floor(len(backgrounds)*1/3)
+trainBackgroundsMax = len(backgrounds)
+testBackgroundsMin = 0
+testBackgroundsMax = math.floor(len(backgrounds)*1/3)
 
-    responsesPersons = np.ones(persons2len, np.uint8)
-    responsesBackgrounds = np.zeros(backgrounds2len, np.uint8)
-    responses = np.append(responsesPersons, responsesBackgrounds)
+personsTrainData = persons[trainPersonsMin:trainPersonsMax]
+personsTestData = persons[testPersonsMin:testPersonsMax]
 
-    responsesPersons = None
-    responsesBackgrounds = None
-    persons2len = None
-    backgrounds2len = None
-    return hog, responses
+backgroundsTrainData = backgrounds[trainBackgroundsMin:trainBackgroundsMax]
+backgroundsTestData = backgrounds[testBackgroundsMin:testBackgroundsMax]
 
+responsesPersons = np.ones(len(personsTrainData), np.uint8)
+responsesBackgrounds = np.zeros(len(backgroundsTrainData), np.uint8)
+responses = np.append(responsesPersons, responsesBackgrounds)
 
-hog, responses = getArrays()
+trainHog = np.append(personsTrainData.flatten(), backgroundsTrainData.flatten())
+trainHog = np.reshape(trainHog, (-1,5880))
 
 print('Starting to train')
 
 svm = train.createSVM()
-train.train(svm, hog, responses)
+train.train(svm, trainHog, responses)
 train.save(svm, 'saveData.dat')
+
+testHog = np.append(personsTestData.flatten(), backgroundsTestData.flatten())
+testHog = np.reshape(testHog, (-1, 5880))
+
+testData = np.array(testHog, dtype=np.float32)
+testResponse = svm.predict(testData)[1].ravel()
+
+print('Porcentaje de personas correcto: ')
+correctPersons = np.count_nonzero(testResponse[:testPersonsMax - testPersonsMin])/(testPersonsMax - testPersonsMin)*100
+print("{:.2f}".format(correctPersons))
+print('Porcentaje de personas incorrecto: ')
+print("{:.2f}".format(100 - correctPersons))
+
+print('Porcentaje de fondos correcto: ')
+wrongBackgrounds = np.count_nonzero(testResponse[testPersonsMax - testPersonsMin:])/(testBackgroundsMax-testBackgroundsMin)*100
+print("{:.2f}".format(100 - wrongBackgrounds))
+print('Porcentaje de fondos incorrecto: ')
+print("{:.2f}".format(wrongBackgrounds))
